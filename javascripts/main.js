4a026e0e876265778d539d02bc1c798508bf9f5a
Anessa Ortner <anessa.ortner@gmail.com>
2017-05-02 19:24:59 -0500

    working register functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -87,7 +87,22 @@ $(document).ready(function(){
 		});
 	});
 
-	
+	//*******************************************************
+	//USER REGISTERING - LOGIN FUNCTIONS
+	//*******************************************************
+
+	$("#registerButton").click(() => {
+		let email = $("#inputEmail").val();
+		let password = $("#inputPassword").val();
+		let username = $("#inputUsername").val();
+
+		let user = {email, password};
+		FbAPI.registerUser(user).then((response) => {
+			console.log("register response", response);
+		}).catch((error) => {
+			console.log("register user error: ", error);
+		});
+	});
 
 
 
---
+++ javascripts/userCrud.js
@@ -0,0 +1,5 @@
+var FbAPI = ((oldFbAPI) => {
+
+
+	return oldFbAPI;
+})(FbAPI || {});
